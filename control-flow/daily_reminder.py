514b90dba9f061494e2d013d3859adefa4031c05
MikiMesfin <mikimesfin45@yahoo.com>
2024-06-30 19:13:53 +0300

    add files to folder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,9 +1,8 @@
 #Prompt for a Single Task
-task = input("Enter your task: ").lower()
-priority = input("priority (high/medium/low): ").lower()
+task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ").lower()  # Ensure case-insensitive input
 time_bound = input("Is it time_bound? (yes/no): ").lower()
 
-#Process the Task Based on Priority and Time Sensitivity
 match priority:
     case "high":
         if time_bound == "yes":
@@ -11,13 +10,12 @@ match priority:
         else:
             print(f"Reminder: '{task}' is a high priority task.")
     case "medium":
-            print(f"Reminder: '{task}' is a medium priority task.")
+        print(f"Reminder: '{task}' is a medium priority task.")
     case "low":
         if time_bound == "no":
-            print(f"Reminder: '{task}' is a low priority task.Consider completing it when you have free time.")
+            print(f"Reminder: '{task}' is a low priority task. Consider completing it when you have free time.")
         else:
-            print(f"Reminder: '{task}' is a low priority task.")
+            print(f"Reminder: '{task}' is a low priority time-bound task.")
     case _:
         print("Invalid priority or time-bound entered. Please enter the correct input.")
-
         
